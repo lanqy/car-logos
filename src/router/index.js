bdc13008eae6dcefdb581169709e5e05159359f7
Lan Qingyong <lanqy1314@gmail.com>
2017-05-02 17:18:13 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import List from '../components/list'
+import Detail from '../components/detail'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/car/:country',
       name: 'List',
       component: List,
+      props: true
+    },
+    {
+      path: '/car/:cate/:id',
+      name: 'Detail',
+      component: Detail,
       props: true
     }
   ],
